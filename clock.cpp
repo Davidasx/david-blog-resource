04a6aed83ac81b57f54a008dc1752d4a1ec37b01
Davidasx <81460770+Davidasx@users.noreply.github.com>
2022-05-01 23:03:17 +0800

    k49suc4cbr

--- clock.cpp
+++ clock.cpp
@@ -1,24 +1,36 @@
-#include <graphics.h>
-#include <conio.h>
+#include<graphics.h>
+#include<conio.h>
+#include<windows.h>
+#include<bits/stdc++.h>
+using namespace std;
 double PI=3.1415926535897932;
 int main(){
-	initgraph(400,400);
-	circle(200,200,100);
-	int SECONDS=30;
+	int R,G,B,SECONDS;
+	cin>>R>>G>>B>>SECONDS;
+	initgraph(200,200);
+	setfillcolor(R*256*256+G*256+B);
+	solidrectangle(0,0,200,200);
+	setfillcolor(0);
+	solidcircle(100,100,60);
+	setfillcolor(255*256*256+255*256+255);
+	solidcircle(100,100,50);
 	for(int i=30-SECONDS;i<=30;i++){
 		double A=cos(i*1.00*PI/30.00);
 		double B=sin(i*1.00*PI/30.00);
 		setlinecolor(BLUE);
-		line(200,200,217,210);
+		line(100,100,109,105);
 		setlinecolor(YELLOW);
-		line(200,200,200,160);
+		line(100,100,100,80);
 		setlinecolor(RED);
-		line(200,200,200-B*60,200+A*60);
+		line(100,100,100-B*30,100+A*30);
 		Sleep(1000);
-		setlinecolor(BLACK);
-		line(200,200,200-B*60,200+A*60);
+		setlinecolor(WHITE);
+		line(100,100,100-B*30,200+A*30);
+		if(GetAsyncKeyState('A')){
+			closegraph();
+			return 0;
+		}
 	}
-	_getch();
 	closegraph();
 	return 0;
 }
