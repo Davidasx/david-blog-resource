99f9d1cdf4057e31a62d923b07221a0c27359076
Davidasx <81460770+Davidasx@users.noreply.github.com>
2022-05-16 23:18:35 +0800

    jphw8vwy6s

--- matcher.cpp
+++ matcher.cpp
@@ -7,7 +7,9 @@ int main(){
 	cin>>target_a>>target_b;
 	while(1){
 		for(int i=1;i<=5;i++){
-			A=B=-1;
+			A=rand()%28,B=rand()%24;
+			A+=1;if(A>14) A+=12;
+			B+=15;if(B>26) B+=14;
 			while((target_a!=0&&A!=target_a)||(target_b!=0&&B!=target_b)){
 				A=rand()%28,B=rand()%24;
 				A+=1;if(A>14) A+=12;
